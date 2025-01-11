65300f0ef41cb8772c02a5609f51ac3f9ae7f015
naitiklovestech <singhalnaitik22@gmail.com>
2025-01-12 01:14:04 +0530

    Almost there

--- app/src/main/java/com/example/imgstenography/MainActivity.java
+++ app/src/main/java/com/example/imgstenography/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.imgstenography;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button enc,dec,abt;
+    ViewFlipper vf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        enc = findViewById(R.id.encd);
+        dec = findViewById(R.id.decd);
+
+        enc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent temp = new Intent(MainActivity.this, Encoder.class);
+                startActivity(temp);
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent temp = new Intent(MainActivity.this, Decoder.class);
+                startActivity(temp);
+            }
+        });
+
+        vf = findViewById(R.id.vf);
+        int images[] = {R.drawable.bellaso_cipher, R.drawable.caesar_cipher_encryption, R.drawable.dorabella_cipher};
+
+        for(int i=0; i<images.length; i++){
+            flipper(images[i]);
+        }
+    }
+
+    public void flipper(int images){
+
+        ImageView test = new ImageView(this);
+        test.setBackgroundResource(images);
+        vf.addView(test);
+        vf.setFlipInterval(3000);
+        vf.setAutoStart(true);
+        vf.setInAnimation(this, android.R.anim.slide_in_left);
+        vf.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
